3031f155f057a8a13d0b68fd203f403bf25f9ded
Kristaps Kerpe <kristaps.kerpe@gmail.com>
2017-01-14 17:58:04 +0100

    add sass and compass to project

--- Gemfile
+++ Gemfile
@@ -1,4 +1,10 @@
 source 'https://rubygems.org'
 
+# framework
 gem 'sinatra'
+
+# web server
 gem 'thin'
+
+# css extension
+gem 'sass', '~> 3.4', '>= 3.4.21'
--- public/js/alphabet.js
+++ public/js/alphabet.js
@@ -1,5 +1,5 @@
-$(function() {
-    cookieExists = getCookie();
+$(function () {
+    var cookieExists = getCookie();
 
     if (cookieExists == null || cookieExists == undefined) {
         $("#preloader").removeClass("hidden");
@@ -10,29 +10,29 @@ $(function() {
 
     }
 
-	$("#rotation_arrow").click(function() {
-	    var letter_list = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+    $("#rotation_arrow").click(function () {
+        var letter_list = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
         var random_letter = generate_letter(letter_list);
         insert_letter(random_letter);
     });
 
-    function setCookie () {
-        Cookies.set('alphabetApp', 'visited', { expires: 0.1, path: '' });
-    };
+    function setCookie() {
+        Cookies.set('alphabetApp', 'visited', {expires: 0.1, path: ''});
+    }
 
-    function getCookie () {
+    function getCookie() {
         return Cookies.get('alphabetApp');
-    };
+    }
 
-	function insert_letter(random_letter) {
-		var text = $('h1#body_title').text();
-		$("h1#body_title").replaceWith('<h1 id=body_title>' + random_letter + '</h1>');
-	};
+    function insert_letter(random_letter) {
+        var bodyTitle = $('h1#body_title');
+        bodyTitle.replaceWith('<h1 id=body_title>' + random_letter + '</h1>');
+    }
 
     function generate_letter(letter_list) {
-    	// add condition that previous letter dont generate again
-		return letter_list[Math.floor(Math.random() * letter_list.length)];
-    };
+        // add condition that previous letter dont generate again
+        return letter_list[Math.floor(Math.random() * letter_list.length)];
+    }
 
     function loadLoadingBar() {
         var widthOfLoadingBar = 0;
@@ -48,19 +48,20 @@ $(function() {
             widthOfLoadingBar = widthOfLoadingBar + incrementWidthValueBy;
             document.getElementById('progress_span').style.width = widthOfLoadingBar + '%';
         }, incrementSpeedValue);
-    };
+    }
 
     function loadComplete() {
         // preloader out
         var preloaderOutTl = new TimelineMax();
+        var preloaderElement = $('#preloader');
 
         preloaderOutTl
-            .to($('.progress'), 0.3, {y: 100, autoAlpha: 0, ease:Back.easeIn})
-            .to($('.txt-perc'), 0.3, {y: 100, autoAlpha: 0, ease:Back.easeIn}, 0.1)
+            .to($('.progress'), 0.3, {y: 100, autoAlpha: 0, ease: Back.easeIn})
+            .to($('.txt-perc'), 0.3, {y: 100, autoAlpha: 0, ease: Back.easeIn}, 0.1)
             .set($('body'), {className: '-=is-loading'})
-            .to($('#preloader'), 0.7, {yPercent: 100, ease:Power4.easeInOut})
-            .set($('#preloader'), {className: '+=is-hidden'});
+            .to(preloaderElement, 0.7, {yPercent: 100, ease: Power4.easeInOut})
+            .set(preloaderElement, {className: '+=is-hidden'});
 
         return preloaderOutTl;
-    };
+    }
 });
